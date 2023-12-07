9a0066140235f84106901b887a7d9c4136f1f6b7
bholmesdev <hey@bholmes.dev>
2023-12-07 11:32:14 -0500

    deps(ex): add node adapter

--- examples/form/astro.config.mjs
+++ examples/form/astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from "astro/config";
 import simpleStackForm from "simple-stack-form";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [simpleStackForm(), react()]
+  integrations: [simpleStackForm(), react()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
