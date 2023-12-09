7a3eb6b2118467ffc6d9e17518ab8c3383dbee20
bholmesdev <hey@bholmes.dev>
2023-12-09 17:07:22 -0500

    chore: a11y warning

--- examples/form/src/components/Signup.tsx
+++ examples/form/src/components/Signup.tsx
@@ -3,37 +3,38 @@ import { Form, Input, useCreateFormContext } from "./Form";
 import { createForm } from "simple:form";
 
 export const signup = createForm({
-  username: z.string().min(2),
-  optIn: z.boolean().optional(),
+	username: z.string().min(2),
+	optIn: z.boolean().optional(),
 });
 
 export default function Signup() {
-  const context = useCreateFormContext(signup.validator);
+	const context = useCreateFormContext(signup.validator);
 
-  return (
-    <Form
-      className="flex flex-col gap-2 items-start"
-      context={context}
-      validator={signup.validator}
-    >
-      <FormGroup>
-        <label htmlFor="name">Name</label>
-        <Input id="name" {...signup.inputProps.username} />
-      </FormGroup>
-      <FormGroup>
-        <label htmlFor="optIn">Opt in</label>
-        <Input id="optIn" {...signup.inputProps.optIn} />
-      </FormGroup>
-      <button
-        disabled={context.value.hasFieldErrors}
-        className="bg-purple-700 rounded px-5 py-2 disabled:bg-purple-900"
-      >
-        Submit
-      </button>
-    </Form>
-  );
+	return (
+		<Form
+			className="flex flex-col gap-2 items-start"
+			context={context}
+			validator={signup.validator}
+		>
+			<FormGroup>
+				<label htmlFor="name">Name</label>
+				<Input id="name" {...signup.inputProps.username} />
+			</FormGroup>
+			<FormGroup>
+				<label htmlFor="optIn">Opt in</label>
+				<Input id="optIn" {...signup.inputProps.optIn} />
+			</FormGroup>
+			<button
+				type="submit"
+				disabled={context.value.hasFieldErrors}
+				className="bg-purple-700 rounded px-5 py-2 disabled:bg-purple-900"
+			>
+				Submit
+			</button>
+		</Form>
+	);
 }
 
 function FormGroup({ children }: { children: React.ReactNode }) {
-  return <div className="flex gap-3 items-center">{children}</div>;
+	return <div className="flex gap-3 items-center">{children}</div>;
 }
