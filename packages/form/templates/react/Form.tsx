a792a442a159eabbe42703a532a6373ddb7d1a79
bholmesdev <hey@bholmes.dev>
2023-12-10 12:07:28 -0500

    refactor: spread inputProps at end

--- packages/form/templates/react/Form.tsx
+++ packages/form/templates/react/Form.tsx
@@ -104,7 +104,6 @@ export function Input(inputProps: ComponentProps<"input"> & { name: string }) {
 	return (
 		<>
 			<input
-				{...inputProps}
 				onBlur={async (e) => {
 					const value = e.target.value;
 					if (value === "") return;
@@ -115,6 +114,7 @@ export function Input(inputProps: ComponentProps<"input"> & { name: string }) {
 					const value = e.target.value;
 					formContext.validateField(inputProps.name, value, validator);
 				}}
+				{...inputProps}
 			/>
 			{validationErrors?.map((e) => (
 				<p className="text-red-400" key={e}>
