8a20a07f854d546276a7c12c5fdf610fa43a941e
bholmesdev <bholmesdev@gmail.com>
2024-01-23 07:22:38 -0500

    wip: attempt CollectSuspended wrapper

--- packages/stream/components/index.ts
+++ packages/stream/components/index.ts
@@ -1,2 +1,2 @@
-export { default as ResolveSuspended } from "./ResolveSuspended.astro";
+export { default as CollectSuspended } from "./CollectSuspended.astro";
 export { default as Suspense } from "./Suspense.astro";
