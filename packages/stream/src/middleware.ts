cad3b183b533e1802685a9085ff7634b585bc044
bholmesdev <bholmesdev@gmail.com>
2024-02-03 18:59:12 -0500

    feat: support parallel and nested suspense

--- packages/stream/src/middleware.ts
+++ packages/stream/src/middleware.ts
@@ -1,28 +1,63 @@
-import { defineMiddleware } from "astro/middleware";
+import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware(async ({ locals }, next) => {
-	locals.stream = {
-		components: [],
-	};
-	let response = await next();
+type SuspendedChunk = {
+	chunk: string;
+	idx: number;
+};
+
+export const onRequest = defineMiddleware(async (ctx, next) => {
+	const response = await next();
+	// ignore non-HTML responses
 	if (!response.headers.get("content-type")?.startsWith("text/html")) {
 		return response;
 	}
 
+	let streamController: ReadableStreamDefaultController<SuspendedChunk>;
+
 	async function* render() {
+		// Thank you owoce!
+		// https://gist.github.com/lubieowoce/05a4cb2e8cd252787b54b7c8a41f09fc
+		const stream = new ReadableStream<SuspendedChunk>({
+			start(controller) {
+				streamController = controller;
+			},
+		});
+
+		let curId = 0;
+		const pending = new Set<Promise<string>>();
+
+		ctx.locals.suspend = (promise) => {
+			const idx = curId++;
+			pending.add(promise);
+			promise
+				.then((chunk) => {
+					streamController.enqueue({ chunk, idx });
+					pending.delete(promise);
+				})
+				.catch((e) => {
+					streamController.error(e);
+				});
+			return idx;
+		};
+
 		// @ts-expect-error ReadableStream does not have asyncIterator
-		for await (let chunk of response.body) {
+		for await (const chunk of response.body) {
 			yield chunk;
 		}
-		for (let [idx, component] of locals.stream.components.entries()) {
-			yield `<template data-suspense-id=${JSON.stringify(
-				idx,
-			)}>${await component}</template>
+
+		if (!pending.size) return streamController.close();
+
+		// @ts-expect-error ReadableStream does not have asyncIterator
+		for await (const { chunk, idx } of stream) {
+			yield `<template data-suspense=${JSON.stringify(idx)}>${chunk}</template>
 <script>
-	const template = document.querySelector(\`template[data-suspense-id="${idx}"]\`).content;
-	const dest = document.querySelector(\`simple-suspense[data-suspense-id="${idx}"]\`);
+(() => {
+	const template = document.querySelector(\`[data-suspense="${idx}"]\`).content;
+	const dest = document.querySelector(\`[data-suspense-fallback="${idx}"]\`);
 	dest.replaceWith(template);
+})();
 </script>`;
+			if (!pending.size) return streamController.close();
 		}
 	}
 
