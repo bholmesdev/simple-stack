cfddb82c10c806e1f2c9461a297a2f48cae94759
bholmesdev <bholmesdev@gmail.com>
2024-02-19 08:33:16 -0500

    refactor: readability pass

--- packages/stream/src/middleware.ts
+++ packages/stream/src/middleware.ts
@@ -24,53 +24,14 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 		},
 	});
 
+	/** Incrementing ID for each suspense boundary */
 	let curId = 0;
-	const pending = new Map<number, Promise<string>>();
-	const suspendedChunks = new Map<number, string>();
+	/** Map of all suspense promises in-flight by ID */
+	const pendingChunks = new Map<number, Promise<string>>();
+	/** Map of suspense content waiting to be sent by ID */
+	const flushableChunks = new Map<number, string>();
+	/** Map from child suspense boundaries to parents by ID */
 	const ancestors = new Map<number, number>();
-
-	function getRootId(id: number) {
-		let rootId = id;
-		while (ancestors.has(rootId)) {
-			rootId = ancestors.get(rootId)!;
-		}
-		return rootId;
-	}
-
-	function flushSuspendedChunk({ id, chunk }: SuspendedChunk) {
-		const parentId = SuspenseStorage.getStore()?.id;
-		if (parentId !== undefined) {
-			ancestors.set(id, parentId);
-		}
-		const rootId = getRootId(id);
-		if (rootId === id) {
-			suspendedChunks.set(id, chunk);
-			// Send the chunk after a delay to allow
-			// nested suspense boundaries to flush.
-			setTimeout(() => {
-				streamController.enqueue({ id, chunk: suspendedChunks.get(id)! });
-				suspendedChunks.delete(id);
-			}, FLUSH_THRESHOLD);
-			return;
-		}
-
-		const flushedChunk = suspendedChunks.get(rootId)?.replace(
-			new RegExp(
-				// Replace fallbacks with resolved content.
-				`${fallbackMarkerStart(id)}.*?${fallbackMarkerEnd(id)}`,
-				"s",
-			),
-			chunk,
-		);
-		if (flushedChunk) {
-			pending.delete(id);
-			suspendedChunks.set(rootId, flushedChunk);
-		} else {
-			// Root was already sent to the client.
-			// Enqueue to resolve the child via client JS.
-			streamController.enqueue({ id, chunk });
-		}
-	}
 
 	ctx.locals.suspend = async (promiseCb) => {
 		const id = curId++;
@@ -81,7 +42,7 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 
 		const parentId = SuspenseStorage.getStore()?.id;
 		const parentPromise =
-			parentId !== undefined ? pending.get(parentId) : undefined;
+			parentId !== undefined ? pendingChunks.get(parentId) : undefined;
 
 		const promise = new Promise<string>((resolve) => {
 			if (!parentPromise) {
@@ -92,19 +53,57 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 			// This ensures the parent is sent to the client first.
 			parentPromise.then(() => resolve(basePromise));
 		});
-		pending.set(id, promise);
+		pendingChunks.set(id, promise);
 
 		// Render content without a fallback if resolved quickly.
 		const child = await Promise.race([promise, sleep(FLUSH_THRESHOLD)]);
 
 		if (typeof child === "string") {
-			pending.delete(id);
+			pendingChunks.delete(id);
 			return { render: "content", value: child };
 		}
 
 		promise
-			.then(async (baseChunk) => {
-				flushSuspendedChunk({ id, chunk: baseChunk });
+			.then(async (chunk) => {
+				const parentId = SuspenseStorage.getStore()?.id;
+				if (parentId !== undefined) {
+					ancestors.set(id, parentId);
+				}
+				const rootId = getRootId(id);
+				if (rootId === id) {
+					flushableChunks.set(id, chunk);
+					// Briefly wait for any children to resolve before sending parent.
+					// This allows the child to render its content server-side
+					// rather than sending a fallback to resolve client-side.
+					// The React team calls this server-side resolution "flushing."
+					setTimeout(() => {
+						const flushedChunk = flushableChunks.get(id) ?? chunk;
+						streamController.enqueue({ id, chunk: flushedChunk });
+						flushableChunks.delete(id);
+					}, FLUSH_THRESHOLD);
+					return;
+				}
+
+				const flushableChunk = flushableChunks.get(rootId);
+				if (flushableChunk) {
+					// Parent has not been sent to the client yet.
+					// Replace the fallback with the child content.
+					flushableChunks.set(
+						rootId,
+						flushableChunk.replace(
+							new RegExp(
+								`${fallbackMarkerStart(id)}.*?${fallbackMarkerEnd(id)}`,
+								"s",
+							),
+							chunk,
+						),
+					);
+					pendingChunks.delete(id);
+				} else {
+					// Parent was already sent to the client.
+					// Enqueue to render the child client-side.
+					streamController.enqueue({ id, chunk });
+				}
 			})
 			.catch((e) => {
 				streamController.error(e);
@@ -112,6 +111,15 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 
 		return { render: "fallback", id };
 	};
+
+	/** Walk up the ancestor tree to find the root suspense boundary */
+	function getRootId(id: number) {
+		let rootId = id;
+		while (ancestors.has(rootId)) {
+			rootId = ancestors.get(rootId)!;
+		}
+		return rootId;
+	}
 
 	const response = await next();
 
@@ -127,8 +135,11 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 		}
 
 		// Immediately close the stream if Suspense was not used.
-		if (!pending.size) return streamController.close();
+		if (!pendingChunks.size) return streamController.close();
 
+		// Send a script to query for the fallback and replace with content.
+		// This is grouped into a global __SIMPLE_SUSPENSE_INSERT
+		// to reduce the payload size for multiple suspense boundaries.
 		yield `<script>window.__SIMPLE_SUSPENSE_INSERT = function (idx) {
 	var template = document.querySelector('[data-suspense="' + idx + '"]').content;
 	var dest = document.querySelector('[data-suspense-fallback="' + idx + '"]');
@@ -140,8 +151,8 @@ export const onRequest = defineMiddleware(async (ctx, next) => {
 			yield `<template data-suspense=${id}>${chunk}</template>` +
 				`<script>window.__SIMPLE_SUSPENSE_INSERT(${id});</script>`;
 
-			pending.delete(id);
-			if (!pending.size) return streamController.close();
+			pendingChunks.delete(id);
+			if (!pendingChunks.size) return streamController.close();
 		}
 	}
 
